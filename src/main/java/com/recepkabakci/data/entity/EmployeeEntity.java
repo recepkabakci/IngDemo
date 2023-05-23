049a38fef0afb23d176a6dd91b5d5be1807c94bf
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-05-23 16:00:02 +0200

    EmployeeEntity

---
+++ src/main/java/com/recepkabakci/data/entity/EmployeeEntity.java
@@ -0,0 +1,31 @@
+package com.recepkabakci.data.entity;
+
+import javax.persistence.*;
+
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@Builder
+@Log4j2
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_name")
+    private String emailId;
+
+    public EmployeeEntity(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+}
